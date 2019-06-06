1754430675390baf525cd665abd2dcb9a8dd3578
Benna93 <elbachiri.ben.aissa@gmail.com>
2019-06-06 14:09:27 +0200

    pagination trial non working

--- script.js
+++ script.js
@@ -3,13 +3,14 @@ function append(parent, el) {
   }
 
 const content = document.getElementById('content'); // Get the list where we will place our beer
-const url = 'https://api.punkapi.com/v2/beers'; 
+  
 
+function displayItems(page){
+    const url = `https://api.punkapi.com/v2/beers?page=${page}&per_page=80`; 
 fetch(url) // Call the fetch function passing the url of the API as a parameter
 .then((resp) => resp.json()) // Transform the data into json
 .then(function(data) {
-    console.log(data)
-    data.forEach(function(Beer) { // Map through the results and for each run the code below
+      data.forEach(function(Beer) { // Map through the results and for each run the code below
       let card = document.createElement('div');
       card.classList.add(`card`); //  Create the elements we need
       let title = document.createElement('h3');
@@ -20,5 +21,29 @@ fetch(url) // Call the fetch function passing the url of the API as a parameter
       append(content, card);
       append(card, title);
       append(card, tag);
-    })
-  })
+
+      card.addEventListener("click",showMore);
+      function showMore(){
+        for(i=0; i<Beer.length;i++){
+            let info = document.createElement('p');
+            info.innerHTML = Beer[i];
+            append(card, info);
+            console.log(Beer);
+        }    
+        };   
+      });
+})
+};
+
+const previous = document.getElementById("Previous");
+const next = document.getElementById("Next");
+
+displayItems(1);
+previous.addEventListener("click",goBack);
+function goBack(){
+    displayItems(page - 1);
+}
+next.addEventListener("click",nextPage);
+function nextPage(){
+    displayItems(page + 1);
+}
