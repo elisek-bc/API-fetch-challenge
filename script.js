fdec6721599a330c0c58497f63d92a5f438e245c
elisek-bc <elisekppns@gmail.com>
2019-06-06 16:38:40 +0200

    Ben Time, working toggle mode information

--- script.js
+++ script.js
@@ -3,47 +3,114 @@ function append(parent, el) {
   }
 
 const content = document.getElementById('content'); // Get the list where we will place our beer
-  
 
-function displayItems(page){
-    const url = `https://api.punkapi.com/v2/beers?page=${page}&per_page=80`; 
+const url = `https://api.punkapi.com/v2/beers?page=1&per_page=25`; 
 fetch(url) // Call the fetch function passing the url of the API as a parameter
 .then((resp) => resp.json()) // Transform the data into json
 .then(function(data) {
-      data.forEach(function(Beer) { // Map through the results and for each run the code below
-      let card = document.createElement('div');
-      card.classList.add(`card`); //  Create the elements we need
-      let title = document.createElement('h3');
-      card.classList.add(`card-title`);
-      let tag = document.createElement('h4');
-      title.innerHTML = `${Beer.name}`; // Make the HTML of our span to be the first and last name of our Beer
-      tag.innerHTML = `${Beer.tagline}`;
-      append(content, card);
-      append(card, title);
-      append(card, tag);
+    data.forEach(function(Beer) { // Map through the results and for each run the code below
+        var card = document.createElement('div');
+        card.classList.add(`card`); //  Create the elements we need
+        let title = document.createElement('h3');
+        card.classList.add(`card-title`);
+        let tag = document.createElement('h4');
+        title.innerHTML = `${Beer.name}`; // Make the HTML of our span to be the first and last name of our Beer
+        tag.innerHTML = `${Beer.tagline}`;
+        append(content, card);
+        append(card, title);
+        append(card, tag);
 
-      card.addEventListener("click",showMore);
-      function showMore(){
-        for(i=0; i<Beer.length;i++){
-            let info = document.createElement('p');
-            info.innerHTML = Beer[i];
-            append(card, info);
-            console.log(Beer);
-        }    
-        };   
-      });
-})
+        let subtitle = document.createElement('h5');
+        subtitle.innerHTML = `Description`;
+        subtitle.classList.add("display");
+
+        let description = document.createElement('p');
+        description.innerHTML = `${Beer.description}`;
+        description.classList.add("display");
+        
+        let subtitle2 = document.createElement('h5');
+        subtitle2.innerHTML = `Food pairing`;
+        subtitle2.classList.add("display");
+
+        let food = document.createElement('p');
+        food.innerHTML = `${Beer.food_pairing}`;
+        food.classList.add("display");
+
+        append(card, subtitle);
+        append(card, description);
+        append(card, subtitle2);
+        append(card, food);
+
+        card.addEventListener("click",showMore);
+        function showMore(event){
+            description.classList.toggle("display");
+            subtitle.classList.toggle("display");
+            food.classList.toggle("display");
+            subtitle2.classList.toggle("display");
+
+        }
+    });
+});     
+
+
+
+
+const Random = document.getElementById("Random");
+Random.addEventListener("click", random)
+
+// SHOW RANDOM BEER EXTRA
+function random() {
+content.innerHTML='<h1>Beers</h1>';
+const random =  "https://api.punkapi.com/v2/beers/random";
+fetch(random) // Call the fetch function passing the url of the API as a parameter
+.then((resp) => resp.json()) // Transform the data into json
+.then(function(data) { 
+    // Map through the results and for each run the code below
+        var card = document.createElement('div');
+        card.classList.add(`card`); //  Create the elements we need
+        let title = document.createElement('h3');
+        card.classList.add(`card-title`);
+        let tag = document.createElement('h4');
+        title.innerHTML = `${data[0].name}`; // Make the HTML of our span to be the first and last name of our Beer
+        tag.innerHTML = `${data[0].tagline}`;
+        append(content, card);
+        append(card, title);
+        append(card, tag);
+
+        let subtitle = document.createElement('h5');
+        subtitle.innerHTML = `Description`;
+        subtitle.classList.add("display");
+
+        let description = document.createElement('p');
+        description.innerHTML = `${data[0].description}`;
+        description.classList.add("display");
+        
+        let subtitle2 = document.createElement('h5');
+        subtitle2.innerHTML = `Food pairing`;
+        subtitle2.classList.add("display");
+
+        let food = document.createElement('p');
+        food.innerHTML = `${data[0].food_pairing}`;
+        food.classList.add("display");
+
+        append(card, subtitle);
+        append(card, description);
+        append(card, subtitle2);
+        append(card, food);
+
+        card.addEventListener("click",showMore);
+        function showMore(event){
+            description.classList.toggle("display");
+            subtitle.classList.toggle("display");
+            food.classList.toggle("display");
+            subtitle2.classList.toggle("display");
+
+        }
+    });
+    
+    window.scroll({
+        top:0,
+        left:0,
+        behavior: 'smooth'
+    });
 };
-
-const previous = document.getElementById("Previous");
-const next = document.getElementById("Next");
-
-displayItems(1);
-previous.addEventListener("click",goBack);
-function goBack(){
-    displayItems(page - 1);
-}
-next.addEventListener("click",nextPage);
-function nextPage(){
-    displayItems(page + 1);
-}
