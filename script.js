18c12fb623abe57e4527f6089e6d45e6c78861e7
elisek-bc <elisekppns@gmail.com>
2019-06-06 11:03:14 +0200

    Add cards and styling

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function append(parent, el) {
     return parent.appendChild(el); // Append the second parameter(element) to the first one
   }
 
-const ul = document.getElementById('Beers'); // Get the list where we will place our beer
+const content = document.getElementById('content'); // Get the list where we will place our beer
 const url = 'https://api.punkapi.com/v2/beers'; 
 
 fetch(url) // Call the fetch function passing the url of the API as a parameter
@@ -10,10 +10,15 @@ fetch(url) // Call the fetch function passing the url of the API as a parameter
 .then(function(data) {
     console.log(data)
     data.forEach(function(Beer) { // Map through the results and for each run the code below
-      let li = document.createElement('li'), //  Create the elements we need
-          span = document.createElement('span');
-      span.innerHTML = `${Beer.name} ${Beer.name}`; // Make the HTML of our span to be the first and last name of our Beer
-      append(li, span);
-      append(ul, li);
+      let card = document.createElement('div');
+      card.classList.add(`card`); //  Create the elements we need
+      let title = document.createElement('h3');
+      card.classList.add(`card-title`);
+      let tag = document.createElement('h4');
+      title.innerHTML = `${Beer.name}`; // Make the HTML of our span to be the first and last name of our Beer
+      tag.innerHTML = `${Beer.tagline}`;
+      append(content, card);
+      append(card, title);
+      append(card, tag);
     })
   })
